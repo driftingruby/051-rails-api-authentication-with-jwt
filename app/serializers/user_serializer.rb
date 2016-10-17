fc2fc6407e8e8d5eb6a2795808217aa3f3a450ac
Dave Kimura <dave@k-innovations.net>
2016-10-16 22:36:29 -0400

    050

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,12 +1,12 @@
 class UserSerializer < ApplicationSerializer
-  attributes :id, :first_name, :last_name #, :show
+  attributes :id, :first_name, :last_name, :show
   attribute :email, if: :current_user
   attribute :edit_link, if: :current_user_is_owner
   has_many :phones
 
-  # def show
-  #   UsersController.render(:show, assigns: { user: object}, layout: false ).squish
-  # end
+  def show
+    UsersController.render(:show, assigns: { user: object}, layout: false ).squish
+  end
 
 
   def edit_link
